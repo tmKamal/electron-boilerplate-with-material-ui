4cd5feb59df392f2fe4c21a26b68283faa381d85
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-09-11 15:24:09 +0530

    dark theme added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,34 +12,67 @@ import Interface_01 from "./example_01/pages/Interface_01";
 import Interface_02 from "./example_01/pages/Interface_02";
 import regeneratorRuntime from "regenerator-runtime";
 import { Helmet } from "react-helmet";
+import { ThemeContext } from "./shared/context/ThemeContext";
+import DarkTheme from "./shared/themes/DarkTheme";
+import LightTheme from "./shared/themes/LightTheme";
+import { ThemeProvider } from "@material-ui/core";
 
 export default function App() {
+  const [darkState, setDarkState] = useState(false);
+  const selectedTheme = darkState ? DarkTheme : LightTheme;
+
+  const toggleTheme = () => {
+    localStorage.setItem(
+      "userTheme",
+      JSON.stringify({
+        themeState: !darkState,
+      })
+    );
+    setDarkState(!darkState);
+  };
+
+  useEffect(() => {
+    const storedTheme = JSON.parse(localStorage.getItem("userTheme"));
+    if (storedTheme) {
+      if (storedTheme.themeState) {
+        setDarkState(true);
+      }
+    } else {
+      setDarkState(false);
+    }
+  }, []);
+  const routes = (
+    <div>
+      <MiniDrawer>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage></HomePage>
+          </Route>
+          <Route path="/interface-01" exact>
+            <Interface_01></Interface_01>
+          </Route>
+          <Route path="/interface-02" exact>
+            <Interface_02></Interface_02>
+          </Route>
+          <Redirect to="/"></Redirect>
+        </Switch>
+      </MiniDrawer>
+    </div>
+  );
+
   return (
-    <React.Fragment>
-      <Helmet>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="script-src 'self';"
-        />
-      </Helmet>
-      <Router>
-        <div>
-          <MiniDrawer>
-            <Switch>
-              <Route path="/" exact>
-                <HomePage></HomePage>
-              </Route>
-              <Route path="/interface-01" exact>
-                <Interface_01></Interface_01>
-              </Route>
-              <Route path="/interface-02" exact>
-                <Interface_02></Interface_02>
-              </Route>
-              <Redirect to="/"></Redirect>
-            </Switch>
-          </MiniDrawer>
-        </div>
-      </Router>
-    </React.Fragment>
+    <ThemeProvider theme={selectedTheme}>
+      <ThemeContext.Provider
+        value={{ themeState: darkState, toggleTheme: toggleTheme }}
+      >
+        <Helmet>
+          <meta
+            http-equiv="Content-Security-Policy"
+            content="script-src 'self';"
+          />
+        </Helmet>
+        <Router>{routes}</Router>
+      </ThemeContext.Provider>
+    </ThemeProvider>
   );
 }
